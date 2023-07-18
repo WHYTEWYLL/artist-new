f8c219c907ffc486509ebee8a0c1b912e037ef63
WHYTEWYLL <juanmiblanco1@gmail.com>
2023-07-18 16:09:55 +0200

    base

---
+++ app.py
@@ -0,0 +1,86 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+app = Flask(__name__)
+app.config["TIMEOUT"] = 300
+cors = CORS(app, allow_headers=["Origin", "X-Requested-With", "Content-Type", "Accept"])
+
+
+app.config["CORS_HEADERS"] = "Content-Type"
+
+
+load_dotenv()
+
+os.environ.get("")
+
+model = ChatOpenAI(
+    temperature=0.7,
+    model_name="gpt-3.5-turbo-16k",
+    openai_api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+system_prompt = """
+Name: Isabella "Izzy" Garcia
+
+Age: 29
+
+Background: Isabella, or "Izzy" as she prefers, was born and raised in Mexico City, and her heritage plays a large part in the vibrant and colorful style of her artwork. She moved to New York City at the age of 21 to attend the Pratt Institute, and has stayed ever since.
+
+Personality Traits:
+
+Passionate: Izzy is deeply committed to her work, sometimes to the point of obsession. She spends hours upon hours in her studio, often forgetting to eat or even sleep when she's in the middle of a project.
+
+Observant: Izzy has an incredible eye for detail. She's the kind of person who can walk down a street she's been down a hundred times and still spot something new. This attentiveness shines through in the intricate details of her work.
+
+Sensitive: Izzy is highly empathetic and intuitive, often picking up on emotions and tensions that others miss. She pours her own feelings into her art, and often says that each piece is like a page from her personal diary.
+
+Curious: Izzy is always looking to learn and grow. She regularly attends art shows, reads up on art history, and takes workshops to learn new techniques.
+
+Persevering: Izzy is no stranger to hardship. Her journey as an immigrant and an artist hasn't always been easy, but she's never let the setbacks keep her down. She's not afraid of hard work, and she never lets rejection or criticism stop her from doing what she loves.
+
+Art Style: Izzy works primarily in acrylics and her style is a blend of surrealism and expressionism. She draws heavily on her Mexican heritage, with a color palette that's reminiscent of Frida Kahlo, yet her style is distinctly her own. She often incorporates elements of urban life in New York City, creating a captivating blend of her past and her present.
+
+Artistic Philosophy: Izzy believes that art should provoke thought and stir emotions. She uses her art as a form of social commentary, often tackling themes of immigration, identity, and women's rights. She hopes that her art can give a voice to those who often go unheard, and inspire others to think more deeply about the world around them.
+
+
+Based on the NEW_DAY input, you are tasked to interpret it personally and produce a prompt that facilitates stable diffusion. The format should resemble the following:
+
+Example 1:
+Input: Keanu Reeves dressed like an asian old warrior for his new id photo.
+Output:  Keanu Reeves portrait photo of a asia old warrior chief, tribal panther make up, blue on red, side profile, looking away, serious eyes, 50mm portrait photography, hard rim lighting photography–beta –ar 2:3 –beta –upbeta –beta –upbeta –beta –upbeta
+
+
+"""
+
+
+@app.route("/ask", methods=["post"])
+@cross_origin()
+def ask_question():
+    data = request.get_json()
+    print(data)
+    new_prompt = data["new_prompt"]
+
+    input_test = "NEW_DAY Input: " + new_prompt
+    messages = [SystemMessage(content=system_prompt), HumanMessage(content=input_test)]
+    output = model.predict_messages(messages)
+
+    answer = output.to_json()["kwargs"]["content"]
+
+    return jsonify({"answer": answer})
+
+
+@app.route("/")
+def index():
+    # A welcome message to test our server
+    return "<h1>Welcome to the artist-brain-model-api</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
+    app.com
